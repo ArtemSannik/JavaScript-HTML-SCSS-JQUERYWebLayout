48e79cef770e948122f0ca372c2b3813b41991cb
Artem Sannikov <artsannik@mail.ru>
2022-08-08 18:18:36 +0300

    Последний коммит

--- js/index.js
+++ js/index.js
@@ -13,3 +13,30 @@ $('.up-button').on('click',function(){
         scrollTop: 0
     }, "slow");
 });
+
+$("#show-menu").on("click", function() {
+    $("#hidden-menu").animate({
+        "right": 0
+    }, 500);
+});
+
+
+$("#hidden-menu .close").on("click", function() {
+    $("#hidden-menu").animate({
+        "right": "-300px"
+    }, 200);
+});
+
+$(document).ready(function(){
+    $("#slider").slick({
+        dots: false,
+        prevArrow: '<div class="arrow-prev"><i class="fa-solid fa-arrow-left-long"></i></div>',
+        nextArrow: '<div class="arrow-next"><i class="fa-solid fa-arrow-right-long"></i></div>',
+        inifinite: true,
+        slidesToShow: 2
+
+       
+
+    });
+});
+
